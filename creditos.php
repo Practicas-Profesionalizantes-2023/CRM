d4f257715bd432121dc03ec3e922f2fa14e7fb8d
Fabian Lopez <prof.fabian.lopez@gmail.com>
2023-08-24 17:31:26 -0300

    commit inicial para realizar cambios

---
+++ creditos.php
@@ -0,0 +1,8 @@
+<?php include_once "encabezado.php" ?>
+<div class="row">
+    <div class="col-12">
+        <h2>Créditos de todo lo que use:</h2>
+        
+    </div>
+</div>
+<?php include_once "encabezado.php" ?>
